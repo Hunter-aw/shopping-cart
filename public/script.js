08addddbab1d6677a50646b584f41caa0caecb62
hunter-aw <hunteraweinsheink@gmail.com>
2018-08-13 15:18:44 +0300

    add item functionaility, toggle and clear cart

--- public/script.js
+++ public/script.js
@@ -1,9 +1,18 @@
 var ShoppingCart = function () {
 
   // an array with all of our cart items
-  var cart = [];
+  let cart = [];
+  let cartObj = {
+    cartItems: cart
+  }
 
-  var updateCart = function () {
+  var updateCart = function () { 
+    $('.cart-list').empty()
+    var source = $('#cart-template').html()
+    var template = Handlebars.compile(source)
+    var newHTML = template(cartObj)
+    $('.cart-list').append(newHTML)
+    
     // TODO: Write this function. In this function we render the page.
     // Meaning we make sure that all our cart items are displayed in the browser.
     // Remember to empty the "cart div" before you re-add all the item elements.
@@ -11,18 +20,38 @@ var ShoppingCart = function () {
 
 
   var addItem = function (item) {
+    var price = item.data().price;
+    var name = item.data().name;
+    let newItem = {
+      name: name,
+      price: price
+    }
+    cart.push(newItem);
+    
     // TODO: Write this function. Remember this function has nothing to do with display. 
     // It simply is for adding an item to the cart array, no HTML involved - honest ;-)
   }
+  var cartTotal = function () {
+    var total = 0
+    for (i in cart) {
+      let price = cart[i].price;
+      total += price
+    }
+    $('.total').html(total)
+  }
+
+      
 
   var clearCart = function () {
+    cart.length = 0;
     // TODO: Write a function that clears the cart ;-)
   }
   
   return {
     updateCart: updateCart,
     addItem: addItem,
-    clearCart: clearCart
+    clearCart: clearCart,
+    cartTotal: cartTotal
   }
 };
 
@@ -35,15 +64,19 @@ app.updateCart();
 //--------EVENTS---------
 
 $('.view-cart').on('click', function () {
-  // TODO: hide/show the shopping cart!
-});
+    $('.shopping-cart').toggle()
+  });
 
 $('.add-to-cart').on('click', function () {
   // TODO: get the "item" object from the page
-  app.addItem(item);
+  var $item = $(this).closest('.item')
+  app.addItem($item);
   app.updateCart();
+  app.cartTotal();
 });
 
 $('.clear-cart').on('click', function () {
   app.clearCart();
+  app.updateCart();
+  app.cartTotal();
 });
--- server.js
+++ server.js
@@ -2,4 +2,4 @@ const express = require('express');
 let app = express()
 app.listen(8008)
 app.use(express.static('public'));
-app.use(express.static('node_modules'));
+app.use(express.static('node_modules'));     
