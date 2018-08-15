30cf07ea012b036b5db08ebbd12e0a922af1ca26
hunter-aw <hunteraweinsheink@gmail.com>
2018-08-15 14:06:37 +0300

    modified to use the local server data

--- public/script.js
+++ public/script.js
@@ -2,17 +2,33 @@ var ShoppingCart = function () {
 
   // an array with all of our cart items
   let cart = [];
-  let cartObj = {
-    cartItems: cart
+  // let cartObj = {
+  //   cartItems: cart
+  // }
+  const STORAGE_CART_ID = 'user_cart';
+  const saveCartToStorage = function () {
+    localStorage.setItem(STORAGE_CART_ID, JSON.stringify(cart))
   }
 
-  var updateCart = function () { 
+  const getFromHandleCartStorage = function () {
+    let cart = JSON.parse(localStorage.getItem(STORAGE_CART_ID) || '[]');
+    let storedCart = {
+      cartItems: cart
+    }
+    return storedCart;
+  }
+  const getFromCartStorage = function () {
+    return JSON.parse(localStorage.getItem(STORAGE_CART_ID) || '[]');
+  }
+
+  var updateCart = function () {
     $('.cart-list').empty()
     var source = $('#cart-template').html()
     var template = Handlebars.compile(source)
-    var newHTML = template(cartObj)
+    var newHTML = template(getFromHandleCartStorage())
     $('.cart-list').append(newHTML)
-    
+    cartTotal()
+
     // TODO: Write this function. In this function we render the page.
     // Meaning we make sure that all our cart items are displayed in the browser.
     // Remember to empty the "cart div" before you re-add all the item elements.
@@ -29,24 +45,29 @@ var ShoppingCart = function () {
       id: id,
       idCount: 1
     }
-    if (cart.length > 0) {
-      for (let something of cart) {
-        if (newItem.id === something.id){
-          something.idCount ++;
+    cart = getFromCartStorage()
+    if (!cart.length) {
+      cart.push(newItem)
+      saveCartToStorage()
+    } else { //if cart.length is false
+      for (let item of cart) {
+        if (newItem.id === item.id) {
+          item.idCount++;
+          saveCartToStorage()
           return;
         }
-      } cart.push(newItem)
-        return;
-    } else {
-      cart.push(newItem);
-    } 
+      }
+      cart.push(newItem)
+      saveCartToStorage()
+      return;
+    }
   }
 
   var cartTotal = function () {
     let total = 0
     let price = 0
-    for (let item of cart) {
-      if(item.idCount > 1){
+    for (let item of getFromCartStorage()) {
+      if (item.idCount > 1) {
         price = item.idCount * item.price
         total += price
       } else {
@@ -57,10 +78,11 @@ var ShoppingCart = function () {
     $('.total').html(total)
   }
 
-      
+
 
   var clearCart = function () {
     cart.length = 0;
+    saveCartToStorage()
     // TODO: Write a function that clears the cart ;-)
   }
 
@@ -68,11 +90,12 @@ var ShoppingCart = function () {
     itemId = item.data().id;
     for (let i in cart) {
       if (itemId === cart[i].id) {
-        cart.splice(cart.indexOf(cart[i]),1)
+        cart.splice(cart.indexOf(cart[i]), 1)
       }
     }
+    saveCartToStorage()
   }
-  
+
   return {
     updateCart: updateCart,
     addItem: addItem,
@@ -91,26 +114,26 @@ app.updateCart();
 //--------EVENTS---------
 
 $('.view-cart').on('click', function () {
-    $('.shopping-cart').toggle()
-  });
+  $('.shopping-cart').toggle()
+});
 
 $('.add-to-cart').on('click', function () {
   // TODO: get the "item" object from the page
   const $item = $(this).closest('.item')
   app.addItem($item);
-  app.updateCart()
-  app.cartTotal();
+  app.updateCart();
+  // app.cartTotal();
 });
 
 $('.clear-cart').on('click', function () {
   app.clearCart();
   app.updateCart();
-  app.cartTotal();
+  // app.cartTotal();
 });
 
-$('.cart-list').on('click', '.removeItem', function() {
+$('.cart-list').on('click', '.removeItem', function () {
   const $item = $(this).closest('.cart-item')
   app.removeItem($item);
   app.updateCart();
-  app.cartTotal();
+  // app.cartTotal();
 });
--- server.js
+++ server.js
@@ -3,4 +3,4 @@ let app = express()
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/node_modules'));
 console.log("server is runnin runnin and runnin runnin")
-app.listen(8008)     
+app.listen(8080)     
