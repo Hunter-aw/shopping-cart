40c9dbcdcfaac3d5c1bf177c7f1f29e35a53b01e
hunter-aw <hunteraweinsheink@gmail.com>
2018-08-18 14:06:24 +0300

    updated server

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const express = require('express');
 let app = express()
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/node_modules'));
-console.log("server is runnin runnin and runnin runnin")
+console.log("server is runnin runnin and runnin runnin on http://localhost:8080")
 app.listen(8080)     
